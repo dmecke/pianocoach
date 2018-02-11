84504295e604c425a1d15cd1336c34434788ed12
Daniel Mecke <daniel.mecke@cunningsoft.de>
2018-02-11 21:10:40 +0100

    removed unused property

--- src/js/SongWrapper.ts
+++ src/js/SongWrapper.ts
@@ -1,12 +1,10 @@
 import {OSMD} from "opensheetmusicdisplay";
 import Song from "./Song";
-import {StaffMeasure} from "opensheetmusicdisplay/dist/src/MusicalScore/Graphical/StaffMeasure";
 import SongElement2 from "./SongElement";
 
 export default class SongWrapper {
 
     private osmd: OSMD;
-    private measureList: Array<StaffMeasure>|null = [];
     private errors: number = 0;
 
     constructor(element) {
@@ -17,7 +15,6 @@ export default class SongWrapper {
         return new Promise<void>((resolve) => {
             this.osmd.load(song.xml).then(() => {
                 this.osmd.cursor.cursorElement.style.zIndex = 0;
-                this.measureList = this.osmd.graphic.measureList;
                 resolve();
             });
         });
